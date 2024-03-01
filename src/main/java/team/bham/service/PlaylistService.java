126fb207660327d6380740131016da132754ee5b
Robert Blake <rxb256@student.bham.ac.uk>
2024-03-01 16:51:58 +0000

    Create playlist in database from Spotify Tracks and AudioFeatures objects

---
+++ src/main/java/team/bham/service/PlaylistService.java
@@ -0,0 +1,60 @@
+package team.bham.service;
+
+import java.util.ArrayList;
+import se.michaelthelin.spotify.model_objects.specification.AudioFeatures;
+import se.michaelthelin.spotify.model_objects.specification.Track;
+import team.bham.domain.Playlist;
+import team.bham.domain.Song;
+import team.bham.repository.PlaylistRepository;
+import team.bham.repository.SongRepository;
+
+public class PlaylistService {
+
+    private final PlaylistRepository playlistRepository;
+    private final SongRepository songRepository;
+    private final SongService songService;
+
+    public PlaylistService(PlaylistRepository playlistRepository, SongService songService, SongRepository songRepository) {
+        this.playlistRepository = playlistRepository;
+        this.songService = songService;
+        this.songRepository = songRepository;
+    }
+
+    public Playlist createPlaylist(
+        se.michaelthelin.spotify.model_objects.specification.Playlist playlist,
+        ArrayList<Track> tracks,
+        ArrayList<AudioFeatures> audioFeaturesList
+    ) {
+        // Check if playlist is already in the database
+        String spotifyID = playlist.getId();
+        if (playlistRepository.existsBySpotifyID(spotifyID)) {
+            // If yes: return that song
+            return playlistRepository.findPlaylistBySpotifyID(spotifyID);
+        }
+
+        // Create new playlist object
+        Playlist myPlaylist = new Playlist();
+        myPlaylist.setSpotifyID(playlist.getId());
+        myPlaylist.setName(playlist.getName());
+        myPlaylist.setImageURL(playlist.getImages()[0].getUrl());
+        // for each song in playlist
+        if (tracks.size() != audioFeaturesList.size()) {
+            throw new IllegalArgumentException(
+                "tracks and audioFeaturesList are different sizes, respectively: " + tracks.size() + " " + audioFeaturesList.size()
+            );
+        }
+        Song tmpSong;
+        for (int i = 0; i < tracks.size(); i++) {
+            // create song
+            tmpSong = songService.createSong(tracks.get(i), audioFeaturesList.get(i));
+            // link to playlist
+            tmpSong.addPlaylist(myPlaylist);
+            myPlaylist.addSong(tmpSong);
+            songRepository.save(tmpSong);
+        }
+        // save playlist
+        playlistRepository.save(myPlaylist);
+
+        return myPlaylist;
+    }
+}
