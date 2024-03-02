d57496a8c29361c582f8c5433cd5acb28488846d
Robert Blake <rxb256@student.bham.ac.uk>
2024-03-02 21:46:08 +0000

    Temporarily force PlaylistStatsService to make playlist entity
    One-to-one relationship means the playlist and playlist-stats entities must be made at basically the same time

--- src/main/java/team/bham/service/PlaylistStatsService.java
+++ src/main/java/team/bham/service/PlaylistStatsService.java
@@ -1,6 +1,8 @@
 package team.bham.service;
 
 import java.time.Instant;
+import java.util.ArrayList;
+import java.util.Arrays;
 import org.hibernate.boot.model.relational.AuxiliaryDatabaseObject;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -21,17 +23,20 @@ public class PlaylistStatsService {
     private final PlaylistRepository playlistRepository;
     private final SongRepository songRepository;
     private final SongService songService;
+    private final PlaylistService playlistService;
 
     public PlaylistStatsService(
         PlaylistStatsRepository playlistStatsRepository,
         SongService songService,
         SongRepository songRepository,
-        PlaylistRepository playlistRepository
+        PlaylistRepository playlistRepository,
+        PlaylistService playlistService
     ) {
         this.playlistStatsRepository = playlistStatsRepository;
         this.songService = songService;
         this.songRepository = songRepository;
         this.playlistRepository = playlistRepository;
+        this.playlistService = playlistService;
     }
 
     public PlaylistStats createPlaylistStats(
@@ -44,47 +49,51 @@ public class PlaylistStatsService {
         Playlist correspondingPlaylist;
         if (playlistRepository.existsBySpotifyID(spotifyID)) {
             correspondingPlaylist = playlistRepository.findPlaylistBySpotifyID(spotifyID);
-            if (correspondingPlaylist.getPlaylistStats() != null) {
-                return correspondingPlaylist.getPlaylistStats();
-            }
+            return correspondingPlaylist.getPlaylistStats();
         } else {
-            throw new IllegalArgumentException("Playlist must be generated before stats!");
+            // Playlist and stats have to be generated simultaneously because of required on-to-one relationship.
+            correspondingPlaylist =
+                playlistService.createPlaylist(
+                    playlist,
+                    new ArrayList<Track>(Arrays.asList(selectedTracks)),
+                    new ArrayList<AudioFeatures>(Arrays.asList(selectedAudioFeatures))
+                );
         }
 
         // Create new playlist stats object
         PlaylistStats myPlaylistStats = new PlaylistStats();
-        if (selectedTracks.length != 4 || selectedAudioFeatures.length != 4) {
+        /*if (selectedTracks.length != 4 || selectedAudioFeatures.length != 4) {
             throw new IllegalArgumentException("tracks and audio feature arrays must have size 4");
-        }
+        }*/
 
         // Set selected songs
         Song happiestSong = songService.createSong(selectedTracks[0], selectedAudioFeatures[0]);
-        happiestSong.addHappiestPlaylistStats(myPlaylistStats);
+        //happiestSong.addHappiestPlaylistStats(myPlaylistStats);
         myPlaylistStats.setHappiestSong(happiestSong);
-        songRepository.save(happiestSong);
+        //songRepository.save(happiestSong);
 
         Song energeticSong = songService.createSong(selectedTracks[1], selectedAudioFeatures[1]);
-        energeticSong.addHappiestPlaylistStats(myPlaylistStats);
-        myPlaylistStats.setHappiestSong(energeticSong);
-        songRepository.save(happiestSong);
+        //energeticSong.addHappiestPlaylistStats(myPlaylistStats);
+        myPlaylistStats.setEnergeticSong(energeticSong);
+        //songRepository.save(energeticSong);
 
         Song sumsUpSong = songService.createSong(selectedTracks[2], selectedAudioFeatures[2]);
-        sumsUpSong.addHappiestPlaylistStats(myPlaylistStats);
-        myPlaylistStats.setHappiestSong(sumsUpSong);
-        songRepository.save(happiestSong);
+        //sumsUpSong.addHappiestPlaylistStats(myPlaylistStats);
+        myPlaylistStats.setSumsUpSong(sumsUpSong);
+        //songRepository.save(sumsUpSong);
 
         Song anomalousSong = songService.createSong(selectedTracks[3], selectedAudioFeatures[3]);
-        anomalousSong.addHappiestPlaylistStats(myPlaylistStats);
-        myPlaylistStats.setHappiestSong(anomalousSong);
-        songRepository.save(happiestSong);
+        //anomalousSong.addHappiestPlaylistStats(myPlaylistStats);
+        myPlaylistStats.setAnonmalousSong(anomalousSong);
+        //songRepository.save(anomalousSong);
 
         // Update playlist with stats, and vice versa
         myPlaylistStats.setPlaylist(correspondingPlaylist);
         myPlaylistStats.setPlaylistLength(playlist.getTracks().getTotal());
         myPlaylistStats.setLastUpdated(Instant.now());
 
-        playlistRepository.save(correspondingPlaylist);
         playlistStatsRepository.save(myPlaylistStats);
+        playlistRepository.save(correspondingPlaylist);
 
         return myPlaylistStats;
     }
