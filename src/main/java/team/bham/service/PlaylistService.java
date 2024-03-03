9506c86bf28301e953296d221601f07f17b5f7b8
Robert Blake <rxb256@student.bham.ac.uk>
2024-03-03 15:59:15 +0000

    Create playlist stats entity within playlist service rather than vice versa

--- src/main/java/team/bham/service/PlaylistService.java
+++ src/main/java/team/bham/service/PlaylistService.java
@@ -1,6 +1,7 @@
 package team.bham.service;
 
 import java.util.ArrayList;
+import java.util.List;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import se.michaelthelin.spotify.model_objects.specification.AudioFeatures;
@@ -11,22 +12,23 @@ import team.bham.repository.PlaylistRepository;
 import team.bham.repository.SongRepository;
 
 @Service
+@Transactional
 public class PlaylistService {
 
     private final PlaylistRepository playlistRepository;
-    private final SongRepository songRepository;
     private final SongService songService;
+    private final PlaylistStatsService playlistStatsService;
 
-    public PlaylistService(PlaylistRepository playlistRepository, SongService songService, SongRepository songRepository) {
+    public PlaylistService(PlaylistRepository playlistRepository, SongService songService, PlaylistStatsService playlistStatsService) {
         this.playlistRepository = playlistRepository;
         this.songService = songService;
-        this.songRepository = songRepository;
+        this.playlistStatsService = playlistStatsService;
     }
 
     public Playlist createPlaylist(
         se.michaelthelin.spotify.model_objects.specification.Playlist playlist,
-        ArrayList<Track> tracks,
-        ArrayList<AudioFeatures> audioFeaturesList
+        List<Track> tracks,
+        List<AudioFeatures> audioFeaturesList
     ) {
         // Check if playlist is already in the database
         String spotifyID = playlist.getId();
@@ -35,28 +37,27 @@ public class PlaylistService {
             return playlistRepository.findPlaylistBySpotifyID(spotifyID);
         }
 
-        // Create new playlist object
-        Playlist myPlaylist = new Playlist();
-        myPlaylist.setSpotifyID(playlist.getId());
-        myPlaylist.setName(playlist.getName());
-        myPlaylist.setImageURL(playlist.getImages()[0].getUrl());
-        // for each song in playlist
         if (tracks.size() != audioFeaturesList.size()) {
             throw new IllegalArgumentException(
                 "tracks and audioFeaturesList are different sizes, respectively: " + tracks.size() + " " + audioFeaturesList.size()
             );
         }
+
+        // Create new playlist object
+        Playlist myPlaylist = new Playlist();
+        myPlaylist.setSpotifyID(playlist.getId());
+        myPlaylist.setName(playlist.getName());
+        myPlaylist.setImageURL(playlist.getImages()[0].getUrl());
+
         Song tmpSong;
+        // for each song in playlist
         for (int i = 0; i < tracks.size(); i++) {
-            // create song
             tmpSong = songService.createSong(tracks.get(i), audioFeaturesList.get(i));
-            // link to playlist
-            //tmpSong.addPlaylist(myPlaylist);
             myPlaylist.addSong(tmpSong);
-            //songRepository.save(tmpSong);
         }
-        // save playlist
-        //playlistRepository.save(myPlaylist);
+
+        playlistStatsService.createPlaylistStats(myPlaylist);
+        playlistRepository.save(myPlaylist);
 
         return myPlaylist;
     }
--- src/main/java/team/bham/service/PlaylistStatsService.java
+++ src/main/java/team/bham/service/PlaylistStatsService.java
@@ -1,85 +1,50 @@
 package team.bham.service;
 
 import java.time.Instant;
-import java.util.ArrayList;
-import java.util.Arrays;
-import org.hibernate.boot.model.relational.AuxiliaryDatabaseObject;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-import se.michaelthelin.spotify.model_objects.specification.AudioFeatures;
-import se.michaelthelin.spotify.model_objects.specification.Track;
 import team.bham.domain.Playlist;
 import team.bham.domain.PlaylistStats;
-import team.bham.domain.Song;
-import team.bham.repository.PlaylistRepository;
 import team.bham.repository.PlaylistStatsRepository;
 import team.bham.repository.SongRepository;
+import team.bham.service.spotify.PlaylistInsightCalculator;
 
 @Service
-@Transactional
 public class PlaylistStatsService {
 
     private final PlaylistStatsRepository playlistStatsRepository;
-    private final PlaylistRepository playlistRepository;
     private final SongRepository songRepository;
-    private final SongService songService;
-    private final PlaylistService playlistService;
 
-    public PlaylistStatsService(
-        PlaylistStatsRepository playlistStatsRepository,
-        SongService songService,
-        SongRepository songRepository,
-        PlaylistRepository playlistRepository,
-        PlaylistService playlistService
-    ) {
+    public PlaylistStatsService(PlaylistStatsRepository playlistStatsRepository, SongRepository songRepository) {
         this.playlistStatsRepository = playlistStatsRepository;
-        this.songService = songService;
         this.songRepository = songRepository;
-        this.playlistRepository = playlistRepository;
-        this.playlistService = playlistService;
     }
 
-    public PlaylistStats createPlaylistStats(
-        se.michaelthelin.spotify.model_objects.specification.Playlist playlist,
-        Track[] selectedTracks,
-        AudioFeatures[] selectedAudioFeatures
-    ) {
-        // Check if playlist stats have already been generated, and if playlist even exists
-        String spotifyID = playlist.getId();
-        Playlist correspondingPlaylist;
-        if (playlistRepository.existsBySpotifyID(spotifyID)) {
-            correspondingPlaylist = playlistRepository.findPlaylistBySpotifyID(spotifyID);
-            return correspondingPlaylist.getPlaylistStats();
-        } else {
-            // Playlist and stats have to be generated simultaneously because of required on-to-one relationship.
-            correspondingPlaylist =
-                playlistService.createPlaylist(
-                    playlist,
-                    new ArrayList<Track>(Arrays.asList(selectedTracks)),
-                    new ArrayList<AudioFeatures>(Arrays.asList(selectedAudioFeatures))
-                );
+    public PlaylistStats createPlaylistStats(Playlist playlist) {
+        // Check if playlist stats have already been generated
+        PlaylistStats myPlaylistStats = playlist.getPlaylistStats();
+        if (myPlaylistStats != null) {
+            return myPlaylistStats;
         }
 
         // Create new playlist stats object
-        PlaylistStats myPlaylistStats = new PlaylistStats();
+        myPlaylistStats = new PlaylistStats();
+
+        // Calculate stats
+        PlaylistInsightCalculator.AllFeaturesStats stats = new PlaylistInsightCalculator.AllFeaturesStats(playlist);
+        String[] highlightedIDs = PlaylistInsightCalculator.selectHighlightedSongs(playlist, stats);
 
         // Set selected songs
-        Song happiestSong = songService.createSong(selectedTracks[0], selectedAudioFeatures[0]);
-        myPlaylistStats.setHappiestSong(happiestSong);
-        Song energeticSong = songService.createSong(selectedTracks[1], selectedAudioFeatures[1]);
-        myPlaylistStats.setEnergeticSong(energeticSong);
-        Song sumsUpSong = songService.createSong(selectedTracks[2], selectedAudioFeatures[2]);
-        myPlaylistStats.setSumsUpSong(sumsUpSong);
-        Song anomalousSong = songService.createSong(selectedTracks[3], selectedAudioFeatures[3]);
-        myPlaylistStats.setAnonmalousSong(anomalousSong);
+        myPlaylistStats.setHappiestSong(songRepository.findSongBySpotifyID(highlightedIDs[0]));
+        myPlaylistStats.setEnergeticSong(songRepository.findSongBySpotifyID(highlightedIDs[1]));
+        myPlaylistStats.setSumsUpSong(songRepository.findSongBySpotifyID(highlightedIDs[2]));
+        myPlaylistStats.setAnonmalousSong(songRepository.findSongBySpotifyID(highlightedIDs[3]));
 
         // Update playlist with stats, and vice versa
-        myPlaylistStats.setPlaylist(correspondingPlaylist);
-        myPlaylistStats.setPlaylistLength(playlist.getTracks().getTotal());
+        myPlaylistStats.setPlaylist(playlist);
+        myPlaylistStats.setPlaylistLength(playlist.getSongs().size());
         myPlaylistStats.setLastUpdated(Instant.now());
 
         playlistStatsRepository.save(myPlaylistStats);
-        playlistRepository.save(correspondingPlaylist);
 
         return myPlaylistStats;
     }
