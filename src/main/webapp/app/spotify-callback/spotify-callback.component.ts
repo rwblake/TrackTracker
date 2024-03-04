2b993adddc805f0120bb3a2236c4437607157c4c
Joe Haines <jfh245@student.bham.ac.uk>
2024-03-04 17:11:41 +0000

    Update spotify callback page logic

--- src/main/webapp/app/spotify-callback/spotify-callback.component.ts
+++ src/main/webapp/app/spotify-callback/spotify-callback.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'jhi-spotify-callback',
@@ -7,25 +6,19 @@ import { NavigationEnd } from '@angular/router';
   styleUrls: ['./spotify-callback.component.scss'],
 })
 export class SpotifyCallbackComponent implements OnInit {
-  constructor() {}
+  error = false;
 
   ngOnInit(): void {
-    // try to log in automatically
-    this.router.events.subscribe(event => {
-      if (event instanceof NavigationEnd) {
-        this.updateTitle();
-
-        const url = new URL(window.location.href);
-        const params = url.searchParams;
-        if (params.has('state') && (params.has('code') || params.has('error'))) {
-          // Check whether there's an opener to send the code back to
-          if (window.opener != null) {
-            console.log(`sending params '${params.toString()}' to window.opener`, window.opener);
-            window.opener.postMessage(params.toString());
-            // window.close();
-          }
-        }
+    const url = new URL(window.location.href);
+    const params = url.searchParams;
+    if (params.has('state') && (params.has('code') || params.has('error'))) {
+      // Check whether there's an opener window to direct the code back to
+      if (window.opener != null) {
+        window.opener.postMessage(params.toString());
+        window.close();
+        return;
       }
-    });
+    }
+    this.error = true;
   }
 }
