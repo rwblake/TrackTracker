3fd796db9d6884abca61ad75f0d6cf471c900977
Reuben Barnes <rxb208@student.bham.ac.uk>
2024-03-05 11:14:41 +0000

    Display values from HTTP response on webpage

--- src/main/webapp/app/playlist-insights/playlist-insights.component.ts
+++ src/main/webapp/app/playlist-insights/playlist-insights.component.ts
@@ -3,6 +3,7 @@ import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
 import { Title } from '@angular/platform-browser';
 import { APP_NAME } from '../app.constants';
 import { PlaylistInsightsService } from './playlist-insights.service';
+import { PlaylistInsightsResponse } from './playlist-insights-response-interface';
 
 @Component({
   selector: 'jhi-playlist-insights',
@@ -13,6 +14,7 @@ export class PlaylistInsightsComponent implements OnInit {
   urlForm: FormGroup = new FormGroup({ name: new FormControl('', [Validators.required]) });
   linkInput: any;
   constructor(private titleService: Title, private playlistInsightsService: PlaylistInsightsService) {}
+  response: PlaylistInsightsResponse | undefined;
 
   ngOnInit(): void {
     this.titleService.setTitle(APP_NAME + ' - Playlist Insights');
@@ -21,6 +23,7 @@ export class PlaylistInsightsComponent implements OnInit {
   sendLink() {
     // @ts-ignore
     const url: String = this.urlForm.get('name').value;
-    this.playlistInsightsService.sendURL(url).subscribe(value => console.log(value));
+    this.playlistInsightsService.sendURL(url).subscribe(value => (this.response = value));
+    console.log(this.response?.playlistTitle);
   }
 }
