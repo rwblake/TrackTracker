19b09f0bcd97e1f4a3b0edcbc7c29e85230a8394
Robert Blake <rxb256@student.bham.ac.uk>
2024-03-06 01:34:48 +0000

    Test new credentials access using environment variables

--- src/main/java/team/bham/service/spotify/CredentialsParser.java
+++ src/main/java/team/bham/service/spotify/CredentialsParser.java
@@ -9,15 +9,17 @@ public class CredentialsParser {
     /** Reads credentials.txt and returns the Client ID and Client Secret*/
     public static String[] parseCredentials() {
         String[] idAndSecret = { "", "" };
-        try {
-            File file = new File("credentials.txt");
-            Scanner reader = new Scanner(file);
-            idAndSecret[0] = reader.nextLine().trim();
-            idAndSecret[1] = reader.nextLine().trim();
-            reader.close();
-        } catch (FileNotFoundException e) {
-            System.out.println("ERROR: credentials.txt not found");
-        }
+        idAndSecret[0] = System.getenv("SPOTIFY_CLIENT_ID");
+        idAndSecret[1] = System.getenv("SPOTIFY_CLIENT_SECRET");
+        //        try {
+        //            File file = new File("credentials.txt");
+        //            Scanner reader = new Scanner(file);
+        //            idAndSecret[0] = reader.nextLine().trim();
+        //            idAndSecret[1] = reader.nextLine().trim();
+        //            reader.close();
+        //        } catch (FileNotFoundException e) {
+        //            System.out.println("ERROR: credentials.txt not found");
+        //        }
 
         return idAndSecret;
     }
