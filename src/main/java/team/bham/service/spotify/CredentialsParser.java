aa6b5aec2987b3c7fef3880415354f71f894a2d7
Robert Blake <rxb256@student.bham.ac.uk>
2024-03-06 10:04:31 +0000

    Make use of get raw credentials script

--- src/main/java/team/bham/service/spotify/CredentialsParser.java
+++ src/main/java/team/bham/service/spotify/CredentialsParser.java
@@ -8,20 +8,21 @@ public class CredentialsParser {
 
     /** Reads credentials.txt and returns the Client ID and Client Secret*/
     public static String[] parseCredentials() {
-        String[] idAndSecret = { "", "" };
-        //idAndSecret[0] = System.getenv("SPOTIFY_CLIENT_ID");
-        //idAndSecret[1] = System.getenv("SPOTIFY_CLIENT_SECRET");
-        try {
-            File file = new File("credentials.txt");
-            Scanner reader = new Scanner(file);
-            idAndSecret[0] = reader.nextLine().trim();
-            idAndSecret[1] = reader.nextLine().trim();
-            reader.close();
-        } catch (FileNotFoundException e) {
-            System.out.println("ERROR: credentials.txt not found");
-        }
-
-        return idAndSecret;
+        //        String[] idAndSecret = { "", "" };
+        //        idAndSecret[0] = System.getenv("SPOTIFY_CLIENT_ID");
+        //        idAndSecret[1] = System.getenv("SPOTIFY_CLIENT_SECRET");
+        //        try {
+        //            File file = new File("credentials.txt");
+        //            Scanner reader = new Scanner(file);
+        //            idAndSecret[0] = reader.nextLine().trim();
+        //            idAndSecret[1] = reader.nextLine().trim();
+        //            reader.close();
+        //        } catch (FileNotFoundException e) {
+        //            System.out.println("ERROR: credentials.txt not found");
+        //        }
+        //
+        //        return idAndSecret;
+        return getRawCredentials();
     }
 
     /** Raw credentials. BAD PRACTICE AND NEEDS TO BE REMOVED ASAP! ...but needed to get app to work*/
