e02d89e1d115f4190502160b91f1ae026f06e29b
Joe Haines <jfh245@student.bham.ac.uk>
2024-03-07 13:10:27 +0000

    Update friends insights page layout

--- src/main/webapp/app/friends-insights/friends-insights.component.ts
+++ src/main/webapp/app/friends-insights/friends-insights.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { APP_NAME } from '../app.constants';
+import { TimePeriod } from '../time-period-picker/time-period-picker.component';
 
 @Component({
   selector: 'jhi-friends-insights',
@@ -8,9 +9,15 @@ import { APP_NAME } from '../app.constants';
   styleUrls: ['./friends-insights.component.scss'],
 })
 export class FriendsInsightsComponent implements OnInit {
+  selectedTimePeriod?: TimePeriod;
+
   constructor(private titleService: Title) {}
 
   ngOnInit(): void {
     this.titleService.setTitle(APP_NAME + ' - Friend Insights');
+  }
+
+  onTimePeriodChange(period: TimePeriod) {
+    this.selectedTimePeriod = period;
   }
 }
--- src/main/webapp/app/friends-insights/friends-insights.module.ts
+++ src/main/webapp/app/friends-insights/friends-insights.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { FriendsInsightsRoutingModule } from './friends-insights-routing.module';
 import { FriendsInsightsComponent } from './friends-insights.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [FriendsInsightsComponent],
-  imports: [CommonModule, FriendsInsightsRoutingModule],
+  imports: [CommonModule, FriendsInsightsRoutingModule, SharedModule],
 })
 export class FriendsInsightsModule {}
