319c36dca7a41b58d8a1d5cc744c401ea6a536c8
Joe Haines <jfh245@student.bham.ac.uk>
2024-03-08 19:34:16 +0000

    Update friends insights page with fake data card stacks

--- src/main/webapp/app/friends-insights/friends-insights.component.ts
+++ src/main/webapp/app/friends-insights/friends-insights.component.ts
@@ -6,6 +6,7 @@ import { FriendsInsightsService } from './friends-insights.service';
 import { IPopularCategories } from './friends-insights.model';
 import { Account } from '../core/auth/account.model';
 import { AccountService } from '../core/auth/account.service';
+import { CardStackData } from '../card-stack/card-stack.component';
 
 @Component({
   selector: 'jhi-friends-insights',
@@ -19,6 +20,59 @@ export class FriendsInsightsComponent implements OnInit {
   account: Account | null = null;
 
   popularCategories?: IPopularCategories;
+
+  sampleData: CardStackData[] = [
+    {
+      image_url: 'https://i.scdn.co/image/ab67616d00001e02ff9ca10b55ce82ae553c8228',
+      title: 'Eminem',
+      description: '12,325 Streams',
+    },
+    {
+      image_url: 'https://i.scdn.co/image/ab67616d0000b273282b9210352afd2c657ec103',
+      title: 'Billie Eilish',
+      description: '10,619 Streams',
+    },
+    {
+      image_url: 'https://i.scdn.co/image/ab67616d00001e0234f21d3047d85440dfa37f10',
+      title: 'Ariana Grande',
+      description: '4,097 Streams',
+    },
+    {
+      image_url: 'https://i.scdn.co/image/ab67616d00001e02451dc79ccf6dfaa6cc5ce029',
+      title: 'I DONT KNOW HOW BUT THEY FOUND ME',
+      description: '1,290 Streams',
+    },
+    {
+      image_url: 'https://i.scdn.co/image/ab67616d00001e02ff9ca10b55ce82ae553c8228',
+      title: 'Eminem',
+      description: '12,325 Streams',
+    },
+    {
+      image_url: 'https://i.scdn.co/image/ab67616d0000b273282b9210352afd2c657ec103',
+      title: 'Billie Eilish',
+      description: '10,619 Streams',
+    },
+    {
+      image_url: 'https://i.scdn.co/image/ab67616d00001e0234f21d3047d85440dfa37f10',
+      title: 'Ariana Grande',
+      description: '4,097 Streams',
+    },
+    {
+      image_url: 'https://i.scdn.co/image/ab67616d00001e02451dc79ccf6dfaa6cc5ce029',
+      title: 'I DONT KNOW HOW BUT THEY FOUND ME',
+      description: '1,290 Streams',
+    },
+    {
+      image_url: 'https://i.scdn.co/image/ab67616d0000b273282b9210352afd2c657ec103',
+      title: 'Billie Eilish',
+      description: '10,619 Streams',
+    },
+    {
+      image_url: 'https://i.scdn.co/image/ab67616d00001e0234f21d3047d85440dfa37f10',
+      title: 'Ariana Grande',
+      description: '4,097 Streams',
+    },
+  ];
 
   constructor(
     private titleService: Title,
