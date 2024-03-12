f5af9574c3829064515b1cf9b78c4af6ce6e8f82
Matthew Hyde <mxh1131@student.bham.ac.uk>
2024-03-12 22:06:05 +0000

    Get edit profile form data

--- src/main/webapp/app/profile/edit-profile/edit-profile.component.ts
+++ src/main/webapp/app/profile/edit-profile/edit-profile.component.ts
@@ -1,5 +1,9 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { takeUntil } from 'rxjs/operators';
+import { AccountService } from '../../core/auth/account.service';
+import { Account } from '../../core/auth/account.model';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'jhi-edit-profile',
@@ -7,7 +11,9 @@ import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
   styleUrls: ['./edit-profile.component.scss'],
 })
 export class EditProfileComponent implements OnInit {
-  background: string = 'black';
+  private readonly destroy$ = new Subject<void>();
+  account: Account | null = null;
+  //background: string = 'black';
   username: string | undefined;
   pic: string | undefined;
   bio: string | undefined;
@@ -26,19 +32,45 @@ export class EditProfileComponent implements OnInit {
     highContrast: new FormControl('', [Validators.required]),
   });
 
-  constructor(private formBuilder: FormBuilder) {}
+  constructor(private accountService: AccountService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.accountService
+      .getAuthenticationState()
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(account => (this.account = account));
+  }
   onSubmit(): void {
     this.username = this.preferencesForm.get('username')?.value;
+    if (this.username == '') {
+      this.username = this.account?.login;
+    }
     this.pic = this.preferencesForm.get('pic')?.value;
+    if (this.pic == '') {
+      this.pic = 'https://cse.iutoic-dhaka.edu/uploads/img/1601007481_1328.jpg';
+    }
     this.bio = this.preferencesForm.get('bio')?.value;
+    if (this.bio == '') {
+      this.bio = '';
+    }
     this.profilePriv = this.preferencesForm.get('profilePriv')?.value;
+    if (this.profilePriv == 0) {
+      this.profilePriv = 0;
+    }
     this.playlistPriv = this.preferencesForm.get('playlistPriv')?.value;
+    if (this.playlistPriv == 0) {
+      this.playlistPriv = 0;
+    }
     this.darkMode = this.preferencesForm.get('darkMode')?.value;
+    // if (this.darkMode == false) {
+    //   this.background = 'white';
+    // }
     if (this.darkMode == false) {
-      this.background = 'white';
+      this.darkMode = false;
     }
     this.highContrast = this.preferencesForm.get('highContrast')?.value;
+    if (this.highContrast == false) {
+      this.highContrast = false;
+    }
   }
 }
