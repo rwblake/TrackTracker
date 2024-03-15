4100fbaa5441eb876f324d459890c7660c62fad5
Robert Blake <rxb256@student.bham.ac.uk>
2024-03-15 01:45:41 +0000

    Add spotify profile picture when registering AppUser

--- src/main/java/team/bham/service/account/AppUserService.java
+++ src/main/java/team/bham/service/account/AppUserService.java
@@ -1,14 +1,18 @@
 package team.bham.service.account;
 
+import java.io.IOException;
 import java.time.Duration;
 import java.time.Instant;
 import java.util.*;
+import org.apache.hc.core5.http.ParseException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cache.CacheManager;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import se.michaelthelin.spotify.SpotifyApi;
+import se.michaelthelin.spotify.exceptions.SpotifyWebApiException;
 import se.michaelthelin.spotify.model_objects.credentials.AuthorizationCodeCredentials;
 import team.bham.domain.*;
 import team.bham.domain.enumeration.VisibilityPreference;
@@ -17,6 +21,7 @@ import team.bham.security.AuthoritiesConstants;
 import team.bham.service.EmailAlreadyUsedException;
 import team.bham.service.SpotifyIDAlreadyUsedException;
 import team.bham.service.UsernameAlreadyUsedException;
+import team.bham.service.spotify.SpotifyAuthorisationService;
 import team.bham.web.rest.vm.ManagedAppUserVM;
 import tech.jhipster.security.RandomUtil;
 
@@ -32,6 +37,7 @@ public class AppUserService extends team.bham.service.UserService {
     private final SpotifyTokenRepository spotifyTokenRepository;
     private final UserPreferencesRepository userPreferencesRepository;
     private final FeedRepository feedRepository;
+    private final SpotifyAuthorisationService spotifyAuthorisationService;
 
     public AppUserService(
         UserRepository userRepository,
@@ -41,13 +47,15 @@ public class AppUserService extends team.bham.service.UserService {
         AppUserRepository appUserRepository,
         SpotifyTokenRepository spotifyTokenRepository,
         UserPreferencesRepository userPreferencesRepository,
-        FeedRepository feedRepository
+        FeedRepository feedRepository,
+        SpotifyAuthorisationService spotifyAuthorisationService
     ) {
         super(userRepository, passwordEncoder, authorityRepository, cacheManager);
         this.appUserRepository = appUserRepository;
         this.spotifyTokenRepository = spotifyTokenRepository;
         this.userPreferencesRepository = userPreferencesRepository;
         this.feedRepository = feedRepository;
+        this.spotifyAuthorisationService = spotifyAuthorisationService;
     }
 
     protected boolean removeNonActivatedAppUser(AppUser existingUser) {
@@ -71,7 +79,7 @@ public class AppUserService extends team.bham.service.UserService {
         String spotifyID,
         String spotifyDisplayName,
         AuthorizationCodeCredentials spotifyCredentials
-    ) {
+    ) throws IOException, ParseException, SpotifyWebApiException {
         // Check that login isn't used by anyone else.
         // If it is, check that the other account is activated, delete account if not.
         userRepository
@@ -139,12 +147,20 @@ public class AppUserService extends team.bham.service.UserService {
         spotifyToken.setExpires(Instant.now().plus(Duration.ofSeconds(spotifyCredentials.getExpiresIn())));
         spotifyToken.setRefreshToken(spotifyCredentials.getRefreshToken());
 
+        // Get profile information from SpotifyAPI
+        SpotifyApi spotifyApi = this.spotifyAuthorisationService.getSpotifyApiForCurrentUser();
+        spotifyApi.setAccessToken(spotifyToken.getAccessToken());
+        se.michaelthelin.spotify.model_objects.specification.User spotifyUser = spotifyApi.getUsersProfile(spotifyID).build().execute();
+
         // Create an AppUser
         AppUser newAppUser = new AppUser();
         newAppUser.setSpotifyID(spotifyID);
         newAppUser.setBio(appUserVM.getBio());
         newAppUser.setSpotifyUsername(spotifyDisplayName);
         newAppUser.setName("UNUSED FIELD");
+        if (spotifyUser.getImages() != null && spotifyUser.getImages().length != 0) {
+            newAppUser.setAvatarURL(spotifyUser.getImages()[0].getUrl());
+        }
 
         // Create a Feed
         Feed feed = new Feed();
