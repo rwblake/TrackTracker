ce7c72597ae5f72325e8d270aabf5be927c1bfc2
Reuben Barnes <rxb208@student.bham.ac.uk>
2024-03-19 14:56:07 +0000

    Allow users to log in

--- src/main/webapp/app/entities/user/user.model.ts
+++ src/main/webapp/app/entities/user/user.model.ts
@@ -6,7 +6,7 @@ export interface IUser {
 }
 
 export class User implements IUser {
-  constructor(public id: number, public login: string, public firstName: string, public lastName: string) {}
+  constructor(public id: number, public login: string) {}
 }
 
 export function getUserIdentifier(user: IUser): number {
