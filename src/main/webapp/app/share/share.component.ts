9a6ca237ad5d191c12793664848d8bd5af2b9905
shoaib <robe4597@gmail.com>
2024-03-25 08:14:28 +0000

    spotify connection

--- src/main/webapp/app/share/share.component.ts
+++ src/main/webapp/app/share/share.component.ts
@@ -8,7 +8,19 @@ import { APP_NAME } from '../app.constants';
   styleUrls: ['./share.component.scss'],
 })
 export class ShareComponent implements OnInit {
-  constructor(private titleService: Title) {}
+  constructor(private titleService: Title) {
+    true;
+    /*
+    public class SpotifyService {
+
+      public SpotifyStats getUserStats(String username) {
+      // Use Spotify API to fetch user stats
+      // Implementation depends on the Spotify API you're using
+      // Example: spotifyApi.getUserStats(username);
+      return new SpotifyStats(username,  stats  );
+    }
+*/
+  }
 
   ngOnInit(): void {
     this.titleService.setTitle(APP_NAME + ' - Share');
