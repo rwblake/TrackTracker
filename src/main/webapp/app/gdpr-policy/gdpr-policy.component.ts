7168a066a6e8ad3a458474c6729ec32f4225362b
Chris Wilson <cxw298@student.bham.ac.uk>
2024-04-14 15:09:07 +0100

    Use new anchor scroll method which works better in Angular.
    Also added empty js hrefs for a tags which have (click) handlers. This allows them to be picked up by accessibility software.

--- src/main/webapp/app/gdpr-policy/gdpr-policy.component.ts
+++ src/main/webapp/app/gdpr-policy/gdpr-policy.component.ts
@@ -1,24 +1,14 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'jhi-gdpr-policy',
   templateUrl: './gdpr-policy.component.html',
   styleUrls: ['./gdpr-policy.component.scss'],
 })
-export class GdprPolicyComponent implements OnInit {
-  constructor(private router: Router) {}
-
-  ngOnInit(): void {}
-  backToProfile() {
-    this.router.navigate(['/profile/:spotifyUsername']);
-  }
-  notAccept() {
-    let text = document.getElementById('no') as HTMLElement | null;
-    if (text) {
-      text.style.display = 'block';
-    } else {
-      console.error('Text element not found');
-    }
+export class GdprPolicyComponent {
+  public scrollTo(elementId: string): void {
+    document.getElementById(elementId)?.scrollIntoView({
+      behavior: 'smooth',
+    });
   }
 }
