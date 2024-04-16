049af8bde0523c4ed4e15ff827b89fdc4d78f6d0
Robert Blake <rxb256@student.bham.ac.uk>
2024-04-16 11:22:50 +0100

    Show user names on friend recommendations

--- src/main/java/team/bham/domain/FriendRecommendation.java
+++ src/main/java/team/bham/domain/FriendRecommendation.java
@@ -47,7 +47,6 @@ public class FriendRecommendation implements Serializable {
     @NotNull
     @JsonIgnoreProperties(
         value = {
-            "internalUser",
             "userPreferences",
             "spotifyToken",
             "feed",
