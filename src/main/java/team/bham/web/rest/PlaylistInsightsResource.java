e3aba93f2d18596bac202201892a022422383a30
Reuben Barnes <rxb208@student.bham.ac.uk>
2024-04-21 18:11:13 +0100

    Prevent storage of playlists when not logged in
    - This is just achieved by immediately deleting them after processing, which isn't efficient but it's functional

--- src/main/java/team/bham/web/rest/PlaylistInsightsResource.java
+++ src/main/java/team/bham/web/rest/PlaylistInsightsResource.java
@@ -2,6 +2,7 @@ package team.bham.web.rest;
 
 import com.google.gson.Gson;
 import java.io.IOException;
+import java.util.Optional;
 import javax.validation.Valid;
 import org.apache.hc.core5.http.ParseException;
 import org.springframework.http.HttpStatus;
@@ -11,6 +12,8 @@ import org.springframework.web.bind.annotation.*;
 import se.michaelthelin.spotify.SpotifyApi;
 import se.michaelthelin.spotify.exceptions.SpotifyWebApiException;
 import se.michaelthelin.spotify.model_objects.specification.*;
+import team.bham.domain.AppUser;
+import team.bham.repository.PlaylistRepository;
 import team.bham.service.PlaylistService;
 import team.bham.service.UserService;
 import team.bham.service.account.AppUserService;
@@ -50,15 +53,18 @@ public class PlaylistInsightsResource {
         team.bham.domain.Playlist myPlaylist;
         myPlaylist = createPlaylistEntity(spotifyID);
 
-        userService
-            .getUserWithAuthorities()
-            .flatMap(appUserService::getAppUser)
-            .ifPresent(appUser -> {
-                // save a card which tells the user they have just analysed a new playlist
-                cardService.createNewPlaylistCard(appUser, myPlaylist.getId());
-            });
+        // Gets currently logged-in user, if there is one
+        Optional<AppUser> user = userService.getUserWithAuthorities().flatMap(appUserService::getAppUser);
 
         PlaylistInsightsHTTPResponse reply = PlaylistInsightCalculator.getInsights(myPlaylist);
+
+        if (user.isPresent()) {
+            // save a card which tells the user they have just analysed a new playlist
+            cardService.createNewPlaylistCard(user.get(), myPlaylist.getId());
+        } else {
+            // If we're not logged in, we shouldn't permanently store the playlist.
+            playlistService.removePlaylist(myPlaylist);
+        }
 
         Gson gson = new Gson();
         return ResponseEntity.status(HttpStatus.OK).body(gson.toJson(reply));
